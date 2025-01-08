9f53ad8711f563fd6f5e6392aebe4b2b5dbeca17
Joseph Russoniello <jmrusso@bu.edu>
2025-01-08 12:49:26 -0700

    Improved example quality

--- example_project.py
+++ example_project.py
@@ -4,12 +4,13 @@ from improved_webdriver import WebDriver
 #Automatically download and update the chromedriver based on the full path in .env
 update_driver('win64')
 
-#Uses the EasySelenium ability to use webdrivers in a with statement
+#Leverage EasySelenium to manage webdriver using the context manager
 with WebDriver(headless = False,memory_structure = []) as driver:
+    #Use similar syntax to selenium WebDriver to get a page URL
     driver.get('https://www.roblox.com/charts#/?device=computer&country=all')
     
-    #Utilize Easy Selenium to wait 10 seconds for presence of element, and coerce in case of errors in one line
-    divs = driver.find_elements_by_css('div.game-card-name.game-name-title',10,errors = 'coerce')
+    #Attempt to find elements by css, waiting for 10 seconds before returning None (coerced error)
+    divs = driver.find_elements_by_css('div.game-card-name.game-name-title',wait = 10,errors = 'coerce')
     
     #Print top trending names of games in Roblox
     print(list(filter(lambda x:x != "",[div.text for div in divs])))
