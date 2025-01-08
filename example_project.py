8d23e201c630f6519cf11133c9172b19a0048799
Joseph Russoniello <jmrusso@bu.edu>
2025-01-08 12:26:39 -0700

    Improved documentation - Fixed relative path references

--- example_project.py
+++ example_project.py
@@ -1,9 +1,15 @@
 from update_drivers import update_driver
 from improved_webdriver import WebDriver
 
-#update_driver('win64')
+#Automatically download and update the chromedriver based on the full path in .env
+update_driver('win64')
 
+#Uses the EasySelenium ability to use webdrivers in a with statement
 with WebDriver(headless = False,memory_structure = []) as driver:
     driver.get('https://www.roblox.com/charts#/?device=computer&country=all')
-    divs = driver.find_elements_by_css('div.game-card-name.game-name-title',10)
+    
+    #Utilize Easy Selenium to wait 10 seconds for presence of element, and coerce in case of errors in one line
+    divs = driver.find_elements_by_css('div.game-card-name.game-name-title',10,errors = 'coerce')
+    
+    #Print top trending names of games in Roblox
     print(list(filter(lambda x:x != "",[div.text for div in divs])))
